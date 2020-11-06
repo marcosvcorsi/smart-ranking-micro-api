8a75c6d72193218a5c0ff093870b9d78cfed45ee
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-06 20:52:08 -0300

    feat: find players

--- micro-admin-server/src/players/controllers/players.controller.ts
+++ micro-admin-server/src/players/controllers/players.controller.ts
@@ -1,6 +1,25 @@
 import { Controller } from "@nestjs/common";
+import { Ctx, MessagePattern, Payload, RmqContext } from "@nestjs/microservices";
+import { PlayersService } from "../services/players.service";
 
 @Controller()
 export class PlayersController {
 
+  constructor(private readonly playersService: PlayersService) {}
+
+  @MessagePattern('find-players')
+  async findAllCategories(@Payload() id: string, @Ctx() context: RmqContext) {
+    const channel = context.getChannelRef();
+    const originalMessage = context.getMessage();
+
+    try {
+      if(id) {
+        return this.playersService.findById(id);
+      }
+
+      return this.playersService.findAll();
+    } finally {
+      await channel.ack(originalMessage);
+    }
+  }
 }

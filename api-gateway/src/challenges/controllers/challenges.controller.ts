2f93709e17254b791cbda1178c4d7d2e2280c4de
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-08 15:27:17 -0300

    feat: create challenge

--- api-gateway/src/challenges/controllers/challenges.controller.ts
+++ api-gateway/src/challenges/controllers/challenges.controller.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, Controller, Delete, Get, Param, Query } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Delete, Get, Param, Post, Query, UsePipes, ValidationPipe } from '@nestjs/common';
 import { ClientProxyProvider } from 'src/shared/providers/client-proxy.provider';
+import { CreateChallengeDto } from '../dtos/create-challenge.dto';
 
 @Controller('challenges')
 export class ChallengesController {
@@ -28,5 +29,37 @@ export class ChallengesController {
     }
 
     await this.clientProxyProvider.getChallengeInstance().emit('delete-challenge', challenge).toPromise();
+  }
+
+  @Post()
+  @UsePipes(ValidationPipe)
+  async create(@Body() createChallengeDto: CreateChallengeDto) {
+    const players = await this.clientProxyProvider.getAdminServerInstance().send('find-players', '').toPromise();
+
+    createChallengeDto.players.map(player => {
+      const findPlayer = players.find(item => String(item.id) === player);
+
+      if(!findPlayer) {
+        throw new BadRequestException(`Player ${player} not found`);
+      }
+
+      if(findPlayer.category != createChallengeDto.category) {
+        throw new BadRequestException(`Category of ${player} doest not match`);
+      }
+    })
+
+    const challengerIsAPlayer = createChallengeDto.players.find(player => player === createChallengeDto.challenger)
+
+    if(!challengerIsAPlayer) {
+      throw new BadRequestException('The challenge shoud be present in players list')
+    }
+
+    const category = await this.clientProxyProvider.getAdminServerInstance().send('find-categories', createChallengeDto.category).toPromise();
+
+    if(!category) {
+      throw new BadRequestException('Category not found');
+    }
+
+    await this.clientProxyProvider.getChallengeInstance().emit('create-challenge', createChallengeDto).toPromise();
   }
 }

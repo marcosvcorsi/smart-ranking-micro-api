0e40e2559b55b7b481ee0b486a221fefc749cb55
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-08 00:38:30 -0300

    feat: add categories and player dtos

---
+++ api-gateway/src/categories/dtos/category.dto.ts
@@ -0,0 +1,13 @@
+type Event = {
+  name: string;
+  operation: string;
+  value: number;
+}
+
+export class CategoryDto {
+  id: string;
+  name: string;
+  description: string;
+  events: Array<Event>
+}
+

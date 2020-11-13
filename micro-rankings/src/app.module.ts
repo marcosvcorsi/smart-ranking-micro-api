1efcae2f10d731d22278b6a7d0c6ad2a909fd166
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-11-12 21:36:01 -0300

    chore: add config and basic modules

--- micro-rankings/src/app.module.ts
+++ micro-rankings/src/app.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { RankingsModule } from './rankings/rankings.module';
 
 @Module({
-  imports: [RankingsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }),
+    RankingsModule
+  ],
   controllers: [],
   providers: [],
 })
